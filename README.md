aa9c9540bf8435b7877af8116d23c9e4f4f37909
Vladimir Rancic <vladimir.rancic@teliacompany.com>
2024-04-02 15:59:05 +0200

    update project with body-parse and detenv

--- README.md
+++ README.md
@@ -6,13 +6,16 @@ Get a full fake REST API with json-server and zero coding in less than 30 second
 Update reponse.json data with your json object.
 
 ```zsh
-> `git clone git@github.com:Regis011/mock-locally-with-node-js.git project-name`
-
-> `cd project-name`
-
-> `npm install`
-
-> `npm run start`
+> git clone git@github.com:Regis011/mock-locally-with-node-js.git project-name
+```
+```zsh
+> cd project-name
+```
+```zsh
+> npm install
+```
+```zsh
+> npm run start
 ```
 
 chack you reponse heare http://127.0.0.1:3008/api/response
--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+
+dotenv.config();
 const server = jsonServer.create();
+
+// Define "require"
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const middlewares = jsonServer.defaults();
-const response = require('./response.json');
+
+console.log('process.env.PORT', process.env.PORT);
 
 const PORT = process.env.PORT || 3008;
 
 server.use(middlewares);
 
-server.get('/api/response', (req, res) => {
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Test response
+const response = require('./responses/test/test.json');
+server.get('/api/response-test', (req, res) => {
     res.jsonp(response);
 })
+
+const responseParse = require("./responses/test/test-2.json");
+server.post(
+  "/api/response-test-2",
+  jsonParser,
+  (req, res) => {
+    if (req.body.msisdn) {
+      res.jsonp(responseParse);
+    } else {
+      res.send({
+        error: true,
+        message: "Missing number in request body",
+      });
+    }
+  }
+);
 
 server.listen(PORT, () => {
     console.log('process.env.PORT', process.env.PORT);
